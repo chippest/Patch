767d2d95fac66c6455d6a24ce6bd61d33b2ca03c
Chipp <ibtisamaleem6@gmail.com>
2025-03-16 23:01:46 +0500

    remake the bot foundation

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const express = require("express");
-const { port } = require("./config.json");
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
 
-const app = express();
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-app.get("/", (request, response) => {
-  return response.sendFile("index.html", { root: "." });
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-app.listen(port, () =>
-  console.log(`App listening at http://localhost:${port}`)
-);
+// Log in to Discord with your client's token
+client.login(token);
