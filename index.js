95066c6044fc452313fbd47728f4600a2d38ba5c
Chipp <ibtisamaleem6@gmail.com>
2025-03-18 05:28:57 +0500

    fixed some bugs

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ client.once(Events.ClientReady, (readyClient) => {
 
 client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
-  const command = interaction.client.command.get(interaction.commandName);
+  const command = interaction.client.commands.get(interaction.commandName);
 
   if (!command) {
     console.error(`No command matching ${interaction.commandName} was found.`);
