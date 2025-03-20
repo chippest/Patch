a25939dbe36691267a78544b937b982f4093dc18
Chipp <ibtisamaleem6@gmail.com>
2025-03-20 20:12:30 +0500

    learnt message followup

--- commands/utility/secterPing.js
+++ commands/utility/secterPing.js
@@ -8,17 +8,23 @@ module.exports = {
   async execute(interaction) {
     let pong = "# ||pop||";
     await interaction.reply(pong);
-    const interval = setInterval(() => {
+    const interval = setInterval(async () => {
       pong = pong + " ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop||";
       if (pong.length < 2000) {
-        interaction.editReply(pong);
+        await interaction.editReply(pong);
       } else {
         clearInterval(interval);
         clearTimeout(timeout);
+        await interaction.followUp(
+          `Message limit reached, give me nitro so i can send longer messages :P`
+        );
       }
     }, 150);
-    const timeout = setTimeout(() => {
+    const timeout = setTimeout(async () => {
       clearInterval(interval);
+      await interaction.followUp(
+        `Message limit reached, give me nitro so i can send longer messages :P`
+      );
     }, 60000);
   },
 };
