a63c69878f6c5b5066cb2c3a5aaa8f867daf6a1e
Chipp <ibtisamaleem6@gmail.com>
2025-03-20 20:17:02 +0500

    understoo reply, followup, and edit reply

--- commands/utility/secterPing.js
+++ commands/utility/secterPing.js
@@ -8,23 +8,7 @@ module.exports = {
   async execute(interaction) {
     let pong = "# ||pop||";
     await interaction.reply(pong);
-    const interval = setInterval(async () => {
-      pong = pong + " ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop||";
-      if (pong.length < 2000) {
-        await interaction.editReply(pong);
-      } else {
-        clearInterval(interval);
-        clearTimeout(timeout);
-        await interaction.followUp(
-          `Message limit reached, give me nitro so i can send longer messages :P`
-        );
-      }
-    }, 150);
-    const timeout = setTimeout(async () => {
-      clearInterval(interval);
-      await interaction.followUp(
-        `Message limit reached, give me nitro so i can send longer messages :P`
-      );
-    }, 60000);
+    await interaction.followUp(pong + " ||pop||");
+    await interaction.editReply(pong + " ||pop||" + " ||pop||");
   },
 };
