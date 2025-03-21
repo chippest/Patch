0111d663115ad932c3046ea2a63832205597226c
Chipp <ibtisamaleem6@gmail.com>
2025-03-21 21:13:01 +0500

    made roast protection even better

--- commands/fun/roast.js
+++ commands/fun/roast.js
@@ -17,8 +17,19 @@ module.exports = {
     const randomRoast = roasts[Math.floor(Math.random() * roasts.length)];
     let personalizedRoast = randomRoast.replace("{user}", `<@${target.id}>`);
     if (target.id === interaction.client.user.id) {
+      const botResponses = [
+        "Oh come on, you can't roast me!",
+        "Should i call this bravery or are you just dumb?",
+        "Haha,",
+        "Is that the best you've got?",
+        "Nice try, now try again dumbass, ",
+        "You really thought you could roast me? Adorable!",
+        "I appreciate the effort, jk, kys.",
+        "Trying to roast me? Really?",
+      ];
       personalizedRoast =
-        "Nice try, " +
+        botResponses[Math.floor(Math.random() * botResponses.length)] +
+        " " +
         randomRoast.replace("{user}", `<@${interaction.user.id}>`);
     }
     await interaction.reply(personalizedRoast);
